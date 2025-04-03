3a4574202fe0f1570801634e5386f830a27032cb
Sincerely Brittany <sincerelybr@gmail.com>
2025-04-03 15:37:55 -0400

    Accept new clients and assign them a nickname

--- server.py
+++ server.py
@@ -2,10 +2,36 @@ import socket
 import threading
 
 # Server Configuration
-HOST = '127.0.0.1'  # Localhost
-PORT = 55556        # Port number
+SERVER_HOST = '127.0.0.1'  # Localhost
+SERVER_PORT = 55556        # Port number
 
 # Start Server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen()
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((SERVER_HOST, SERVER_PORT))
+server_socket.listen()
+
+active_clients = []
+client_names = []
+
+
+# Accept new clients and assign them a nickname
+def accept_clients():
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Connected with {client_address}")
+
+        client_socket.send("NICK".encode('ascii'))
+        client_name = client_socket.recv(1024).decode('ascii')
+
+        client_names.append(client_name)
+        active_clients.append(client_socket)
+
+        print(f"Nickname: {client_name}")
+        broadcast_message(f"{client_name} joined the chat!".encode('ascii'))
+        client_socket.send("Connected to the chat!".encode('ascii'))
+
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+
+print("Server is running...")
+accept_clients()
