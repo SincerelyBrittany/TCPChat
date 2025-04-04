67a31b3945363b257c02fd5b64368331d7ade679
Sincerely Brittany <sincerelybr@gmail.com>
2025-04-03 22:06:21 -0400

    sends messages to all connect clents

--- server.py
+++ server.py
@@ -13,6 +13,24 @@ server_socket.listen()
 active_clients = []
 client_names = []
 
+# send message to all connected clients
+def broadcast_message(message):
+    for client in active_clients:
+        client.send(message)
+
+# Handle each client connection
+def handle_client(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024)
+            broadcast_message(message)
+        except:
+            client_index = active_clients.index(client_socket)
+            active_clients.remove(client_socket)
+            client_socket.close()
+            client_name = client_names.pop(client_index)
+            broadcast_message(f"{client_name} left the chat.".encode('ascii'))
+            break
 
 # Accept new clients and assign them a nickname
 def accept_clients():
